afc00d358bf2749eedef293d77968b40aa4e6898
florist <79241548+xkcxkc@users.noreply.github.com>
2023-08-29 08:03:38 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'ddd0974f-ee2e-4e71-a3ac-1263c323a1f2';
 
-const proxyIPs = [/*162.159.152.84*/'ctm.xkcxkc.link'];
+const proxyIPs = ['162.159.152.84'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
@@ -164,7 +164,7 @@ async function vlessOverWSHandler(request) {
             const {
                 hasError,
                 message,
-                portRemote = /*2096*/443,
+                portRemote = 2096,
                 addressRemote = '',
                 rawDataIndex,
                 vlessVersion = new Uint8Array([0, 0]),
